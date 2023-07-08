b5fa1bda3df8565f70f1489790c1a4508aaf9658
Austin Daniels <88160796+notaustindaniels@users.noreply.github.com>
2023-07-08 02:07:53 +0000

    Completed API

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
+    const username = "yourusername"; // replace this with your actual username
+
+    // Fetch the initial todo list from the server when the component mounts
+    useEffect(() => {
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`)
+            .then(response => response.json())
+            .then(data => setTodos(data.map(item => item.label)))
+            .catch(error => console.error(error));
+    }, []);
+
+    // Function to update the todo list on the server
+    const updateTodosOnServer = (todos) => {
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
+            method: "PUT",
+            body: JSON.stringify(todos.map(todo => ({ label: todo, done: false }))),
+            headers: { "Content-Type": "application/json" }
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    };
+
+    // Add a new todo
+    const addTodo = (todo) => {
+        const newTodos = [...todos, todo];
+        setTodos(newTodos);
+        updateTodosOnServer(newTodos);
+    };
+
+    // Delete a todo
+    const deleteTodo = (index) => {
+        const newTodos = todos.filter((t, currentIndex) => index !== currentIndex);
+        setTodos(newTodos);
+        updateTodosOnServer(newTodos);
+    };
+
+    // Clear all todos
+    const clearTodos = () => {
+        setTodos([]);
+        fetch(`https://assets.breatheco.de/apis/fake/todos/user/${username}`, {
+            method: "DELETE",
+            headers: { "Content-Type": "application/json" }
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    };
+
+    return (
+		<div id="outer-container">
+			<div id="container">
+				<h1 className="todo-header">To do List</h1>
+				<input
+					id="addToDo"
+					type="text"
+					placeholder="Add to do here"
+					value={inputValue}
+					onChange={(e) => setInputValue(e.target.value)}
+					onKeyDown={(e) => {
+						if (e.key === 'Enter') {
+							addTodo(inputValue);
+							setInputValue("");
+						}
+					}}
+				/>
+				<ul>
+					{todos.map((item, index) => (
+						<li key={index}>
+							<span>
+								<i className="fa fa-trash" onClick={() => deleteTodo(index)}></i>
+							</span>
+							{item}
+						</li>
+					))}
+				</ul>
+			</div>
+			<div id="buttonContainer">
+				<button id="clearButton" className="btn btn-primary" onClick={clearTodos}>Clear All Tasks</button>
+			</div>
 		</div>
 	);
-};
+}
 
 export default Home;
